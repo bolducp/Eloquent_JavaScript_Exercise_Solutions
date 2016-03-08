fd8232fddc1ae25fa1eab857617f053cd44e0d53
Paige Bolduc <paigebolduc@gmail.com>
2016-03-07 18:44:09 -0800

    add refactored solution for deep comparison exercise

--- ch_04/ch04_deep_comparison.js
+++ ch_04/ch04_deep_comparison.js
@@ -7,9 +7,9 @@
  But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
  */
 
-function deepEqual(val1, val2){
+function deepEqual2(val1, val2){
     if (typeof val1 == "object" && val1 != null) {
-        if (typeof val2 == "object" && val2 != 2)  {
+        if (typeof val2 == "object" && val2 != null)  {
 
             if (Object.keys(val1).length != Object.keys(val2).length){
                 return false;
@@ -25,3 +25,36 @@ function deepEqual(val1, val2){
 
     return true;
 }
+
+
+// refactored:
+
+function deepEqual(val1, val2){
+
+  if (areBothObjs(val1, val2)){
+    if (Object.keys(val1).length !== Object.keys(val2).length){
+      return false;
+    } else {
+        for (var property in val1) {
+          return deepEqual(val1[property], val2[property])
+        }
+      }
+  } else {
+    return val1 === val2;
+  }
+}
+
+
+function areBothObjs(val1, val2) {
+  return typeof val1 == "object" && val1 != null && typeof val2 == "object" && val2 != null;
+}
+
+
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
